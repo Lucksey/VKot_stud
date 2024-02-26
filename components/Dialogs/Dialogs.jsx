4078ff3e862828b54e4a3371fc8df2af92c03405
Lucksey <98761938+Lucksey@users.noreply.github.com>
2024-02-26 16:09:53 +0200

    18 lesson; add Dialogs

---
+++ components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Dialogs.module.css";
+
+const Dialogs = (props) => {
+    return (
+      <div>
+          Dialogs  Dialogs
+      </div>
+    )
+}
+
+export default Dialogs;
