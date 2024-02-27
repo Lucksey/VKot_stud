b5fe4c52ba71b2761ccc047d99f85122c7d2c172
Lucksey <98761938+Lucksey@users.noreply.github.com>
2024-02-27 15:36:53 +0200

    23 lesson; add dialogs-dialog-messages-message NavLink

--- components/Dialogs/Dialogs.jsx
+++ components/Dialogs/Dialogs.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import classes from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    <NavLink to='dialogs/1'>Sasha</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='dialogs/2'>Маша</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='dialogs/3'>Глаша</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='dialogs/4'>Vova</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='dialogs/5'>Yura</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='dialogs/6'>Oleg</NavLink>
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>
+                    HI!
+                </div>
+                <div className={classes.message}>
+                    How are you
+                </div>
+                <div className={classes.message}>
+                    rety ewkk
+                </div>
+            </div>
         </div>
     )
 }
