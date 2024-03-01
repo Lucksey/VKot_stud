1cd4e0b1c0fa0ab1f21b3c3e48682c2b5ec56afa
Lucksey <98761938+Lucksey@users.noreply.github.com>
2024-03-01 02:16:08 +0200

    29 lesson; REMOVING ARRAYS to state.js без ДЗ

--- index.js
+++ index.js
@@ -3,34 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-let posts = [{id: 1, message: 'Hi! How are you?', likeCount: 2},
-    {id: 2, message: 'It\'s my first message', likeCount: 25},
-    {id: 3, message: 'It\'s my second message', likeCount: 27},
-    {id: 3, message: 'It\'s my treteee message', likeCount: 29},
-    {id: 3, message: 'It\'s my tresdf234teee message', likeCount: 28989},
-]
-
-let dialogs = [
-    {id: 1, name: 'Sasha'},
-    {id: 2, name: 'Миша'},
-    {id: 3, name: 'Глаша'},
-    {id: 4, name: 'Yaura'},
-    {id: 5, name: 'Oleg'},
-]
-
-let messages = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'How are you'},
-    {id: 3, message: 'эщькере'},
-    {id: 4, message: 'Yo'},
-    {id: 5, message: 'Sky'},
-]
+import state from "./redux/state";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App posts={posts} dialogs={dialogs} messages={messages}/>
+        <App state={state} />
     </React.StrictMode>
 );
 
---
+++ redux/state.js
@@ -0,0 +1,28 @@
+let state = {
+    profilePage: {
+        posts: [{id: 1, message: 'Hi! How are you?', likeCount: 2},
+            {id: 2, message: 'It\'s my first message', likeCount: 25},
+            {id: 3, message: 'It\'s my second message', likeCount: 27},
+            {id: 3, message: 'It\'s my treteee message', likeCount: 29},
+            {id: 3, message: 'It\'s my tresdf234teee message', likeCount: 28989},
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Sasha'},
+            {id: 2, name: 'Миша'},
+            {id: 3, name: 'Глаша'},
+            {id: 4, name: 'Yaura'},
+            {id: 5, name: 'Oleg'},
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How are you'},
+            {id: 3, message: 'эщькере'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Sky'},
+        ]
+    },
+}
+
+export default state;
