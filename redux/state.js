02c8a50d98b9fb2828585a1910ef449c28c2e222
Lucksey <98761938+Lucksey@users.noreply.github.com>
2024-04-03 17:39:26 +0300

    40 lesson. DONE add messages / Messages(Dialogs)

--- redux/state.js
+++ redux/state.js
@@ -54,7 +54,7 @@ let store = {
         } else if (action.type === 'UPDATE_NEW_MESSAGE_BODY') {
             this._state.dialogsPage.newMessageBody = action.body;
             this._callSubscriber(this._state);
-        } else if (action.type === 'SEND-MESSAGE') {
+        } else if (action.type === 'SEND_MESSAGE') {
             let body = this._state.dialogsPage.newMessageBody;
             this._state.dialogsPage.newMessageBody = '';
             this._state.dialogsPage.messages.push({id: 6, message: body});
