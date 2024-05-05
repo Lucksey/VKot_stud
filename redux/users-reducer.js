568621cb2c6bf0be966fc9b04ccaf9946fc62967
Lucksey <98761938+Lucksey@users.noreply.github.com>
2024-05-05 15:30:21 +0300

    55 - React JS практика - pagination, постраничный вывод пользователей

--- redux/users-reducer.js
+++ redux/users-reducer.js
@@ -1,9 +1,15 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+
 
 let initialState = {
-    users: []
+    users: [],
+    pageSize: 100,
+   /* totalUsersCount: 0,*/
+    /*currentPage: 3,*/
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -12,7 +18,6 @@ const usersReducer = (state = initialState, action) => {
         case FOLLOW:
             return {
                 ... state,
-                //users: [...state.users],аналогичная запись
                 users: state.users.map(u => {
                     if (u.id === action.userId){
                         return {...u, followed: true}
@@ -31,8 +36,17 @@ const usersReducer = (state = initialState, action) => {
                 })
             }
         case SET_USERS: {
-            return {...state, users: [...state.users, ...action.users]}
+            return {...state, users: action.users}
         }
+
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, totalUsersCount: action.count}
+        }
+
+        case SET_CURRENT_PAGE: {
+            return {...state, currentPage: action.currentPage}
+        }
+
         default:
             return state;
     }
@@ -41,5 +55,8 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({type:  FOLLOW, userId})
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId})
 export const setUsersAC = (users) => ({type:SET_USERS,users})
+export const setCurrentPageAC = (currentPage) => ({type:SET_CURRENT_PAGE, currentPage })
+export const setTotalUsersCountAC = (totalUsersCount) => ({type:SET_TOTAL_USERS_COUNT, count: totalUsersCount })
+
 
 export default usersReducer;
